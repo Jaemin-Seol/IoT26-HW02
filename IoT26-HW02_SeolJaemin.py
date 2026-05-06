ba2d7dc123fc522837aec91a1b39f80103a7eca1
Seol Jaemin <100254362+Jaemin-Seol@users.noreply.github.com>
2026-05-06 21:49:52 +0900

    Update IoT26-HW02_SeolJaemin.py
    Button pin number changed for easier connection.

--- IoT26-HW02_SeolJaemin.py
+++ IoT26-HW02_SeolJaemin.py
@@ -5,7 +5,7 @@ from gpiozero import Button, LED # or DigitalInputDevice, DigitalOutputDevice
 from signal import pause
 
 led = LED(14) # LED is on GPIO14 = pin8
-button = Button(4) # Button is on GPIO4 = pin7
+button = Button(2) # Button is on GPIO2 = pin3 (The tutorial connects it on 4)
 
 # On when pressed
 # button.when_pressed = led.on
