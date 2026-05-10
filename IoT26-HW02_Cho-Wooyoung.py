12dce17f9904031f0ded045e8ffa63b6ce90280b
sbmax002 <jwooooy002@gmail.com>
2026-05-10 17:42:22 +0900

    chore: add comments

--- IoT26-HW02_Cho-Wooyoung.py
+++ IoT26-HW02_Cho-Wooyoung.py
@@ -1,10 +1,15 @@
+# gpiozero: controls raspberrypi pins
 from gpiozero import Button, LED
 from signal import pause
 
+# led assign gpio pin 14
 led = LED(14)
+# button assign gpio pin 4
 button = Button(4)
 
+# led on while pressed, off when released
 button.when_pressed = led.on
 button.when_released = led.off
 
+# keeps the process alive while waiting for button event
 pause()
